d42b14681953bdfb46fc3c271775f68923cd8454
Marcelo Cortez <marcelohcortez@gmail.com>
2023-05-15 19:31:29 +0200

    Change searchbar background color

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar";
 export default function Header() {
   return (
     <div
-      className="h-64 p-2 bg-gradient-to-b from-orange-300 to-orange-100"
+      className="h-64 p-2 bg-gradient-to-b from-blue-800 to-blue-500"
     >
       <div className="text-center mt-10">
         <h1 className="text-white text-5xl font-bold mb-2">
